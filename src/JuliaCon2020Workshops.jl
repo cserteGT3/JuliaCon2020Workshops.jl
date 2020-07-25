01100079518fa60ff7b98f463aaabb3ea6c9d09a
Tamás Cserteg <csertegt@gmail.com>
2020-07-25 10:50:47 +0200

    basic functionality: get, update, init

--- src/JuliaCon2020Workshops.jl
+++ src/JuliaCon2020Workshops.jl
@@ -1,5 +1,65 @@
 module JuliaCon2020Workshops
 
-# Write your package code here.
+using GitCommand
+using IJulia
+
+export  getworkshop,
+        getworkhops,
+        updateworkshop,
+        updateworkshops,
+        initworkshop
+
+WORKSHOP_DIR = joinpath(dirname(@__DIR__), "workshops")
+
+struct Workshop
+    name::String
+    repo::String
+    projectname::String
+end
+
+export WORKSHOPS
+
+WORKSHOPS = Dict("LearnJulia"=>Workshop("LearnJulia", "https://github.com/dpsanders/LearnJulia2020.git", "LearnJulia2020"),
+            "LightGraphs"=>Workshop("LightGraphs", "https://github.com/matbesancon/lightgraphs_workshop.git", "lightgraphs_workshop"))
+
+function checkworkshopdir(name)
+    wspath = joinpath(WORKSHOP_DIR, name)
+    isdir(wspath) || mkpath(wspath)
+    return nothing
+end
+
+function getworkshop(name)
+    ws = WORKSHOPS[name]
+    checkworkshopdir(ws.projectname)
+    repodir = joinpath(WORKSHOP_DIR, ws.projectname)
+    git() do git
+        run(`$git clone $(ws.repo) $repodir`)
+    end
+end
+
+function updateworkshop(name; force = false)
+    currdir = pwd()
+    ws = WORKSHOPS[name]
+    repodir = joinpath(WORKSHOP_DIR, ws.projectname)
+    isdir(repodir) || error("""Workshop: $name is not cloned! Run getworkshop("$name") first""")
+    cd(repodir)
+    git() do git
+        if force
+            run(`$git stash -qu`)
+        end
+        run(`$git pull -q`)
+    end
+    cd(currdir)
+    return nothing
+end
+
+function initworkshop(name)
+    ws = WORKSHOPS[name]
+    repodir = joinpath(WORKSHOP_DIR, ws.projectname)
+    isdir(repodir) || error("""Workshop: $name is not cloned! Run getworkshop("$name") first""")
+    notebook(detached=true, dir=repodir)
+    return nothing
+end
+
 
 end
